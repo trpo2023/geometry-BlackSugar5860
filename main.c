d4d807899a20f645ed82b656059890d8527dbf38
BlackSugar5860 <124419900+BlackSugar5860@users.noreply.github.com>
2023-03-04 08:30:38 +0700

    ceate circle

--- main.c
+++ main.c
@@ -111,20 +111,9 @@ void circle(char figure[])
         return;
     }
 
-    if (figure[k] != ' ' && figure[k] != ',')
+    if (figure[k-1] != ',')
     {
         printf("Error at column %d: expected ','\n", k);
-    }
-
-    while (figure[k] == ' ')
-    {
-        k++;
-    }
-
-    if (figure[k] != ',')
-    {
-        printf("Error at column %d: expected ','\n", k);
-        return;
     }
 
     while (figure[k] == ' ')
@@ -162,139 +151,26 @@ void circle(char figure[])
     }
 }
 
-void triangle(char figure[])
-{
-    char str[8] = "triangle";
-
-    for (int i = 0; i < 7; i++)
-    {
-        figure[i] = tolower(figure[i]);
-        if (figure[i] != str[i])
-        {
-            char strerr[8];
-            strncpy(strerr, figure, 8);
-            strerr[8] = '\0';
-            printf("Error at column 0: expected 'triangle' instead of %s\n", strerr);
-            return;
-        }
-    }
-
-    int k = 9;
-
-    if (figure[8] != '(')
-    {
-        printf("Error at column %d: expected '('\n", k);
-        return;
-    }
-    k = 10;
-
-    if (figure[9] != '(')
-    {
-        printf("Error at column %d: expected '('\n", k);
-        return;
-    }
-
-    if (check_num(figure, &k) == 0)
-    {
-        return;
-    }
-
-    if (figure[k] == ')' || figure[k-1] != ',')
-    {
-        printf("Error at column %d: expected ','\n", k);
-        return;
-    }
-    
-    if (figure[k] == ',')
-    {
-        k++;
-    }
-
-    if (check_num(figure, &k) == 0)
-    {
-        return;
-    }
-
-    if (figure[k] == ')' || figure[k-1] != ',')
-    {
-        printf("Error at column %d: expected ','\n", k);
-        return;
-    }
-    else if (figure[k] == ',')
-    {
-        k++;
-    }
-
-    if (check_num(figure, &k) == 0)
-    {
-        return;
-    }
-
-    if (figure[k] == ')' || figure[k-1] != ',')
-    {
-        printf("Error at column %d: expected ','\n", k);
-        return;
-    }
-    else if (figure[k] == ',')
-    {
-        k++;
-    }
-
-    if (check_num(figure, &k) == 0)
-    {
-        return;
-    }
-
-    if (figure[k-1] != ')' || figure[k]!= ')')
-    {
-        printf("Error at column %d: expected ')'\n", k);
-    }
-    else
-    {
-        printf("succeed");
-    }
-}
 
 int main()
 {
     char figure1[64];
-    char figure2[64];
-
-    fgets(figure1, 64, stdin);
-    fgets(figure2, 64, stdin);
-
-    figure1[0] = tolower(figure1[0]);
-
-    switch (figure1[0])
+    int n;
+    printf("enter the number of shapes:");
+    scanf("%d\n", &n);
+    while(n>0)
     {
-    case 'c':
-        circle(figure1);
-        break;
-    case 't':
-        triangle(figure1);
-        break;
-    case 'p':
-        // polygon(figure1);
-        break;
-    default:
-        printf("Error at column 0: expected 'Circle', 'Triangle' or 'Polygon'\n");
+    fgets(figure1, 64, stdin);
+    figure1[0] = tolower(figure1[0]);
+        switch (figure1[0])
+        {
+        case 'c':
+            circle(figure1);
+         break;
+        default:
+            printf("Error at column 0: expected 'Circle' or 'Triangle'");
     }
-
-    figure2[0] = tolower(figure2[0]);
-
-      switch (figure2[0])
-      {
-      case 'c':
-          circle(figure2);
-          break;
-      case 't':
-          triangle(figure2);
-          break;
-      case 'p':
-          // polygon(figure2);
-          break;
-      default:
-          printf("Error at column 0: expected 'Circle', 'Triangle' or 'Polygon'\n");
-      } 
+    n--;
+    }
     return 0;
 }
